1b315226c83f83a3fcd3c0e6987f30a80f1dfd86
Rampranav <vrampranav9@gmail.com>
2020-12-05 11:36:52 +0530

    Added Validation and Background Style

--- js/script.js
+++ js/script.js
@@ -14,11 +14,30 @@ function catchError(e) {
     window.location.replace('/servererror.html');
     return;
 }
+//function to check whether string contains any digits
+function validateUserText(userVal) {
+    userVal = userVal.trim();
+    len = userVal.length;
+    count = 0;
+    for (var i = 0; i < len; i++) {
+        if (isNaN(userVal[i]) || userVal[i] === " ") {
+            count++;
+        }
+    }
+    if (count === len) {
+        return true;
+    }
+    return false;
+}
 // TranslateBtn Click Event
 btnTrans.addEventListener('click', () => {
     var userText = userInput.value;
     if (userText.length === 0) {
         alert("Oops! Please enter some text...");
+        return;
+    }
+    if (!validateUserText(userText)) {
+        alert("Input should not contain digits!");
         return;
     }
 
@@ -28,10 +47,9 @@ btnTrans.addEventListener('click', () => {
             outputText.innerText = jsonResponse.contents.translated;
         })
         .catch(e => catchError(e))
-    outputText.style.color = '#272932';
+    outputText.style.color = 'white';
     clearBtn.style.visibility = 'visible';
 });
-
 // Clear Button Click Event
 clearBtn.addEventListener('click', () => {
     userInput.value = '';
